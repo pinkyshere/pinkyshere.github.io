b682850d8a93c58ceb3dcc3c25ec27f6fd114bc5
pinky <76921905+pinkyshere@users.noreply.github.com>
2024-03-28 22:09:59 -0400

    Update app.js

--- assets/particles.js/app.js
+++ assets/particles.js/app.js
@@ -48,7 +48,7 @@ particlesJS('particles-container', {
       "line_linked": {
         "enable": true,
         "distance": 189.39543399174545,
-        "color": "#00ff00",
+        "color": "#c66aeb",
         "opacity": 0.4,
         "width": 1.5782952832645454
       },
